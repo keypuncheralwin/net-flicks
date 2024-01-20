af259da18c21887c69ecaf48be70615866a99ec6
keypuncheralwin <alwingeorge11@gmail.com>
2024-01-20 21:32:02 +1030

    Added recently played carasoul

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { authOptions } from '../utils/auth';
 import Navbar from '../components/Navbar';
 import MovieVideo from '../components/MovieVideo';
+import RecentlyAdded from '../components/RecentlyAdded';
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
@@ -14,6 +15,8 @@ export default async function HomePage() {
   return (
     <div className="p-5 lg:p-0">
       <MovieVideo />
+      <h1 className="text-3xl font-bold ">Recently Added</h1>
+      <RecentlyAdded />
     </div>
   );
 }
