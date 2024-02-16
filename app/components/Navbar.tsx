0b154cfd69f4ce370386136522ec660db3ad934a
keypuncheralwin <alwingeorge11@gmail.com>
2024-02-16 21:12:55 +1030

    Remove search icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Bell, ChevronDown, Search } from 'lucide-react';
+import { Bell, ChevronDown} from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -96,7 +96,6 @@ export default function Navbar(props: NavbarProps) {
         </ul>
       </div>
       <div className="flex items-center gap-x-8">
-        <Search className="w-5 h-5 text-gray-300 cursor-pointer" />
         <Bell className="h-5 w-5 text-gray-300 cursor-pointer" />
         <UserNav userName={props.userName} userEmail={props.userEmail} />
       </div>
